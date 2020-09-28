c519fb48e5d47d95d9740c787a5fe27eb1f26690
Hasan <aamirkhan2009@hotmail.com>
2020-09-28 23:21:21 +0530

    Coin flip game

---
+++ guesscoinflip.py
@@ -0,0 +1,35 @@
+import random
+import time
+
+print('''Can you guess how many heads will show up
+        on flipping a coin 100 times?
+        Using the number of heads that pop up at 40 and 70 flips,
+        try to guess the number of heads at 100 flips!
+        (Press enter to continue)''')
+
+input()
+
+flips = 0
+heads = 0
+
+while (flips < 100):
+    oneflip = random.randint(0, 1)
+    if (oneflip == 1):
+        heads = heads + 1
+    if (flips == 40):
+        print('At 40 flips ' + str(heads) + ' showed up')
+        time.sleep(2)
+    if (flips == 70):
+        print('At 70 flips ' + str(heads) + ' showed up')
+        time.sleep(2)
+    flips = flips + 1
+
+print('Enter your guess below:')
+
+guess = int(input())
+
+if (guess == heads):
+    print('''WOAH! YOU HAD 1 IN 100 CHANCES AND YOU GOT THE NUMBER RIGHT!!!''')
+else:
+    print('Nope :( Heads actually showed up ' + str(heads) + ' times')
+
